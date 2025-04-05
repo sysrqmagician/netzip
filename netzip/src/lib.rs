c4ab7338d753ced0b84bfd2c3d8f86b90aaa4fa6
sysrqmagician <sysrqmagician@proton.me>
2025-04-05 22:46:29 +0200

    refactor!(core): Complete refactor for ergonomics

--- netzip/src/lib.rs
+++ netzip/src/lib.rs
@@ -17,120 +17,147 @@ pub enum Error {
     UnsupportCompression(u16),
 }
 
-pub async fn extract_listing_from_zip_url(
-    url: &str,
-    client: reqwest::Client,
-) -> Result<Vec<CentralDirectoryRecord>, Error> {
-    let min_cde_bytes = ranged_request(
-        url,
-        &format!("bytes=-{}", netzip_parser::EOCD_MIN_SIZE),
-        client.clone(),
-    )
-    .await?;
-
-    let cde = if let Ok(min_out) = CentralDirectoryEnd::parse(&min_cde_bytes) {
-        min_out
-    } else {
-        // There might be a comment, retry with an offset and search for the EOCD
-        let cde_haystack = ranged_request(
-            url,
-            &format!("bytes=-{}", netzip_parser::EOCD_MIN_SIZE + 1024),
-            client.clone(),
-        )
-        .await?;
-
-        CentralDirectoryEnd::find_and_parse(&cde_haystack)
-            .map_err(|e| Error::ParserError(url.into(), e))?
-    };
-
-    let cd_bytes = ranged_request(
-        url,
-        &format!(
-            "bytes={}-{}",
-            cde.central_directory_offset,
-            cde.central_directory_offset + cde.directory_size
-        ),
-        client,
-    )
-    .await?;
-
-    let cd_records = CentralDirectoryRecord::parse_many(&cd_bytes)
-        .map_err(|e| Error::ParserError(url.into(), e))?;
-
-    Ok(cd_records)
+pub struct RemoteZip {
+    url: String,
+    http_client: reqwest::Client,
+    central_directory: Vec<CentralDirectoryRecord>,
 }
 
-pub async fn extract_files_from_zip_url(
-    url: &str,
-    file_paths: Vec<String>,
-    client: reqwest::Client,
-) -> Result<Vec<(LocalFile, Vec<u8>)>, Error> {
-    let cd_records = extract_listing_from_zip_url(url.into(), client.clone()).await?;
-    let needed_cd_records: Vec<&CentralDirectoryRecord> = cd_records
-        .iter()
-        .filter(|x| file_paths.contains(&x.file_name))
-        .collect();
-
-    let mut out = Vec::new();
-
-    for cd_record in needed_cd_records {
-        let lfh_end_offset = cd_record.file_header_offset
-            + netzip_parser::LFH_MIN_SIZE as u32
-            + cd_record.extra_field_length as u32
-            + cd_record.file_name_length as u32
-            + cd_record.file_comment_length as u32;
-        let lfh_bytes = ranged_request(
+impl RemoteZip {
+    pub async fn get_using(url: &str, http_client: reqwest::Client) -> Result<Self, Error> {
+        let min_cde_bytes = ranged_request(
             url,
-            &format!("bytes={}-{}", cd_record.file_header_offset, lfh_end_offset),
-            client.clone(),
+            &format!("bytes=-{}", netzip_parser::EOCD_MIN_SIZE),
+            http_client.clone(),
         )
         .await?;
 
-        let lfh = LocalFile::parse(&lfh_bytes).map_err(|e| Error::ParserError(url.into(), e))?;
+        let cde = if let Ok(min_out) = CentralDirectoryEnd::parse(&min_cde_bytes) {
+            min_out
+        } else {
+            // There might be a comment, retry with an offset and search for the EOCD
+            let cde_haystack = ranged_request(
+                url,
+                &format!("bytes=-{}", netzip_parser::EOCD_MIN_SIZE + 1024),
+                http_client.clone(),
+            )
+            .await?;
 
-        match lfh.compression_method {
-            netzip_parser::CompressionMethod::Deflate
-            | netzip_parser::CompressionMethod::Deflate64 => {
-                let compressed_data: &[u8] = &ranged_request(
-                    url,
-                    &format!(
-                        "bytes={}-{}",
-                        lfh_end_offset,
-                        lfh_end_offset + lfh.compressed_size
-                    ),
-                    client.clone(),
-                )
-                .await?;
+            CentralDirectoryEnd::find_and_parse(&cde_haystack)
+                .map_err(|e| Error::ParserError(url.into(), e))?
+        };
 
-                let mut decoder = DeflateDecoder::new(compressed_data);
-                let mut decoded = Vec::with_capacity(lfh.uncompressed_size as usize);
-                decoder
-                    .read_to_end(&mut decoded)
-                    .map_err(|e| Error::DecompressionError(url.into(), e.to_string()))?;
+        let cd_bytes = ranged_request(
+            url,
+            &format!(
+                "bytes={}-{}",
+                cde.central_directory_offset,
+                cde.central_directory_offset + cde.directory_size
+            ),
+            http_client.clone(),
+        )
+        .await?;
 
-                out.push((lfh, decoded));
-            }
-            netzip_parser::CompressionMethod::Stored => {
-                let data = ranged_request(
-                    url,
-                    &format!(
-                        "bytes={}-{}",
-                        lfh_end_offset,
-                        lfh_end_offset + lfh.uncompressed_size
-                    ),
-                    client.clone(),
-                )
-                .await?;
+        let cd_records = CentralDirectoryRecord::parse_many(&cd_bytes)
+            .map_err(|e| Error::ParserError(url.into(), e))?;
 
-                out.push((lfh, data.into()));
-            }
-            netzip_parser::CompressionMethod::Unsupported(unsupported_id) => {
-                return Err(Error::UnsupportCompression(unsupported_id));
-            }
-        }
+        Ok(Self {
+            url: url.into(),
+            central_directory: cd_records,
+            http_client,
+        })
     }
 
-    return Ok(out);
+    pub async fn get(url: &str) -> Result<Self, Error> {
+        Self::get_using(url, reqwest::Client::new()).await
+    }
+
+    pub fn records(&self) -> &Vec<CentralDirectoryRecord> {
+        &self.central_directory
+    }
+
+    pub fn records_mut(&mut self) -> &mut Vec<CentralDirectoryRecord> {
+        &mut self.central_directory
+    }
+
+    pub async fn download_files(
+        &self,
+        paths: Vec<String>,
+    ) -> Result<Vec<(LocalFile, Vec<u8>)>, Error> {
+        let needed_cd_records: Vec<&CentralDirectoryRecord> = self
+            .central_directory
+            .iter()
+            .filter(|x| paths.contains(&x.file_name))
+            .collect();
+
+        let mut out = Vec::new();
+
+        for cd_record in needed_cd_records {
+            let lfh_end_offset = cd_record.file_header_offset
+                + netzip_parser::LFH_MIN_SIZE as u32
+                + cd_record.extra_field_length as u32
+                + cd_record.file_name_length as u32
+                + cd_record.file_comment_length as u32;
+            let lfh_bytes = &self
+                .ranged_request(&format!(
+                    "bytes={}-{}",
+                    cd_record.file_header_offset, lfh_end_offset
+                ))
+                .await?;
+
+            let lfh = LocalFile::parse(&lfh_bytes)
+                .map_err(|e| Error::ParserError(self.url.clone(), e))?;
+
+            match lfh.compression_method {
+                netzip_parser::CompressionMethod::Deflate
+                | netzip_parser::CompressionMethod::Deflate64 => {
+                    let compressed_data: &[u8] = &self
+                        .ranged_request(&format!(
+                            "bytes={}-{}",
+                            lfh_end_offset,
+                            lfh_end_offset + lfh.compressed_size
+                        ))
+                        .await?;
+
+                    let mut decoder = DeflateDecoder::new(compressed_data);
+                    let mut decoded = Vec::with_capacity(lfh.uncompressed_size as usize);
+                    decoder
+                        .read_to_end(&mut decoded)
+                        .map_err(|e| Error::DecompressionError(self.url.clone(), e.to_string()))?;
+
+                    out.push((lfh, decoded));
+                }
+                netzip_parser::CompressionMethod::Stored => {
+                    let data = &self
+                        .ranged_request(&format!(
+                            "bytes={}-{}",
+                            lfh_end_offset,
+                            lfh_end_offset + lfh.uncompressed_size
+                        ))
+                        .await?;
+
+                    out.push((lfh, data.to_vec()));
+                }
+                netzip_parser::CompressionMethod::Unsupported(unsupported_id) => {
+                    return Err(Error::UnsupportCompression(unsupported_id));
+                }
+            }
+        }
+
+        return Ok(out);
+    }
+
+    async fn ranged_request(&self, range_string: &str) -> Result<Bytes, Error> {
+        self.http_client
+            .get(&self.url)
+            .header("Range", range_string)
+            .send()
+            .await
+            .map_err(|e| Error::NetworkError(self.url.clone(), e))?
+            .bytes()
+            .await
+            .map_err(|e| Error::NetworkError(self.url.clone(), e))
+    }
 }
 
 async fn ranged_request(
